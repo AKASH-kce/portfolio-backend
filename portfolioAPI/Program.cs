0324b6abca2b226f99d2f3fcb35298dee7da6075
AKASH-SETTUKANNU <akash.settukannu@claysys.net>
2025-07-15 22:41:20 +0530

    Update Program.cs

--- portfolioAPI/Program.cs
+++ portfolioAPI/Program.cs
@@ -8,8 +8,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("PortfolioDatabase")
     )
 );
 
