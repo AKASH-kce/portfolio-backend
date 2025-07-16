fad835b39e02d08832cbfe82d38613cf23d91443
AKASH-SETTUKANNU <akash.settukannu@claysys.net>
2025-07-16 09:47:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Run migrations
 export PATH="$PATH:/root/.dotnet/tools"
-dotnet ef database update
+dotnet ef database update --project /app/portfolioAPI.csproj
 
 # Start the app
 exec dotnet portfolioAPI.dll 
